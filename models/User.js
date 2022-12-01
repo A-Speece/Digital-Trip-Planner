da521444b2c7a8048a0f92f46941fb6d95f5f767
Anthony Speece <anthonyspeece@gmail.com>
2022-11-30 18:53:30 -0600

    adding bycrpt to user model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,10 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 class User extends Model {
-  checkPassword() {
-    return true;
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
@@ -35,7 +36,21 @@ User.init(
       },
     },
   },
+
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
